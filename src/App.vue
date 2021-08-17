52e48161b9eb7e5e7c4a065ab17052fa3582d20e
Matthew Xerri <matt.xerri@gmail.com>
2021-08-17 06:45:01 +0200

    feat(blog): media queries

--- src/App.vue
+++ src/App.vue
@@ -7,24 +7,34 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  @import 'styles/media-queries.scss';
 
-#nav {
-  padding: 30px;
+  $green: green;
+  $blue: blue;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: $green;
+    margin-top: 60px;
 
-    &.router-link-exact-active {
-      color: #42b983;
+    @include r('md') {
+      color: $blue;
     }
   }
-}
+
+  #nav {
+    padding: 30px;
+
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+
+      &.router-link-exact-active {
+        color: #42b983;
+      }
+    }
+  }
 </style>
