aec681eff29466fcbc58bad75fa9ef34c23a7288
Matthew Xerri <matt.xerri@gmail.com>
2021-08-22 10:42:13 +0200

    fix(app): style app with variables

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,20 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link to="/myStore">My Store</router-link>
+    <router-link to="/myStore">Blog</router-link>
   </div>
   <router-view/>
 </template>
 
 <style lang="scss">
-  @import 'styles/media-queries.scss';
-
-  $green: green;
-  $blue: blue;
+  @import 'styles/variables.scss';
 
   #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: $green;
+    color: $color-dark;
     margin-top: 60px;
-
-    @include r('md') {
-      color: $blue;
-    }
   }
 
   #nav {
@@ -31,10 +23,10 @@
 
     a {
       font-weight: bold;
-      color: #2c3e50;
+      color: $color-primary;
 
       &.router-link-exact-active {
-        color: #42b983;
+        color: $color-secondary;
       }
     }
   }
